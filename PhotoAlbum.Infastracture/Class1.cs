60d86f8fa0c8c729e95762a6f5b07b185599c564
Andrey <arhip1495@gmail.com>
2017-06-19 10:21:12 +0300

    Добавил структуру для модели.

---
+++ PhotoAlbum.Infastracture/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoAlbum.Infastracture
+{
+    public class Class1
+    {
+    }
+}
